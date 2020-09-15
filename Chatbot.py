5495ece948f2509576d2f66ffdff95c42f283c0b
Rohitpandey29 <31057922+Rohitpandey29@users.noreply.github.com>
2020-09-15 15:01:59 -0400

    taking user input

--- Chatbot.py
+++ Chatbot.py
@@ -1,2 +1,5 @@
 print("Hello! My name is Jarvis.")
 print("I was created in 2020")
+print('Please, remind me your name.')
+name = input() # reading a name
+print('What a great name you have,' + name )
