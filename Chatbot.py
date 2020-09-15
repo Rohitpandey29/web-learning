b9f357cb86728213563136708eec850359cbc78b
Rohitpandey29 <31057922+Rohitpandey29@users.noreply.github.com>
2020-09-15 15:27:07 -0400

    fix

--- Chatbot.py
+++ Chatbot.py
@@ -2,4 +2,4 @@ print("Hello! My name is Jarvis.")
 print("I was created in 2020")
 print('Please, remind me your name.')
 name = input() # reading a name
-print('What a great name you have,' + name )
+print('What a great name you have, ' + name + '!' )
